11a8f8dbff72aaab8be926bcde0063a8cd5da70b
linjinze <linjinze999@163.com>
2018-09-04 21:08:09 +0800

    [form]add base form

--- src/pages/functions/forms/FuncFormsBase.vue
+++ src/pages/functions/forms/FuncFormsBase.vue
@@ -1,10 +1,200 @@
 <template>
-  <div>基础表单</div>
+  <div>
+    <el-card>
+      请参考<a href="http://element-cn.eleme.io/#/zh-CN/component/form" style="color: #409eff">Element Form表单</a>。
+    </el-card>
+    <br/>
+    <el-card>
+      <el-tabs v-model="activeName">
+        <el-tab-pane label="表单Form" name="first">
+          <el-card shadow="hover" style="width: 700px;">
+            <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px">
+              <el-form-item label="活动名称" prop="name">
+                <el-input v-model="ruleForm.name"></el-input>
+              </el-form-item>
+              <el-form-item label="活动区域" prop="region">
+                <el-select v-model="ruleForm.region" placeholder="请选择活动区域">
+                  <el-option label="区域一" value="shanghai"></el-option>
+                  <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="活动时间" required>
+                <el-col :span="11">
+                  <el-form-item prop="date1">
+                    <el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.date1"
+                                    style="width: 100%;"></el-date-picker>
+                  </el-form-item>
+                </el-col>
+                <el-col class="line" :span="2">-</el-col>
+                <el-col :span="11">
+                  <el-form-item prop="date2">
+                    <el-time-picker type="fixed-time" placeholder="选择时间" v-model="ruleForm.date2"
+                                    style="width: 100%;"></el-time-picker>
+                  </el-form-item>
+                </el-col>
+              </el-form-item>
+              <el-form-item label="即时配送" prop="delivery">
+                <el-switch v-model="ruleForm.delivery"></el-switch>
+              </el-form-item>
+              <el-form-item label="活动性质" prop="type">
+                <el-checkbox-group v-model="ruleForm.type">
+                  <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
+                  <el-checkbox label="地推活动" name="type"></el-checkbox>
+                  <el-checkbox label="线下主题活动" name="type"></el-checkbox>
+                  <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
+                </el-checkbox-group>
+              </el-form-item>
+              <el-form-item label="特殊资源" prop="resource">
+                <el-radio-group v-model="ruleForm.resource">
+                  <el-radio label="线上品牌商赞助"></el-radio>
+                  <el-radio label="线下场地免费"></el-radio>
+                </el-radio-group>
+              </el-form-item>
+              <el-form-item label="活动形式" prop="desc">
+                <el-input type="textarea" v-model="ruleForm.desc"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+                <el-button @click="resetForm('ruleForm')">重置</el-button>
+              </el-form-item>
+            </el-form>
+          </el-card>
+        </el-tab-pane>
+        <el-tab-pane label="上传Upload" name="second">
+          <el-card shadow="hover">
+            <el-upload
+              class="upload-demo"
+              action="https://jsonplaceholder.typicode.com/posts/"
+              :on-preview="handlePreview"
+              :on-remove="handleRemove"
+              :before-remove="beforeRemove"
+              multiple
+              :limit="3"
+              :on-exceed="handleExceed"
+              :file-list="fileList">
+              <el-button size="small" type="primary">点击上传</el-button>
+              <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+            </el-upload>
+          </el-card>
+          <br/>
+          <el-card shadow="hover">
+            <el-upload
+              class="upload-demo"
+              drag
+              action="https://jsonplaceholder.typicode.com/posts/"
+              multiple>
+              <i class="el-icon-upload"></i>
+              <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+              <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+            </el-upload>
+          </el-card>
+        </el-tab-pane>
+        <el-tab-pane label="穿梭框Transfer" name="third">
+          <el-card shadow="hover">
+            <el-transfer
+              filterable
+              :filter-method="filterMethod"
+              filter-placeholder="请输入城市拼音"
+              v-model="value2"
+              :data="data2">
+            </el-transfer>
+          </el-card>
+        </el-tab-pane>
+      </el-tabs>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'FuncFormBase'
+  name: 'FuncFormBase',
+  data () {
+    const generateData2 = _ => {
+      const data = []
+      const cities = ['上海', '北京', '广州', '深圳', '南京', '西安', '成都']
+      const pinyin = ['shanghai', 'beijing', 'guangzhou', 'shenzhen', 'nanjing', 'xian', 'chengdu']
+      cities.forEach((city, index) => {
+        data.push({
+          label: city,
+          key: index,
+          pinyin: pinyin[index]
+        })
+      })
+      return data
+    }
+    return {
+      activeName: 'first',
+      ruleForm: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      rules: {
+        name: [
+          {required: true, message: '请输入活动名称', trigger: 'blur'},
+          {min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur'}
+        ],
+        region: [
+          {required: true, message: '请选择活动区域', trigger: 'change'}
+        ],
+        date1: [
+          {type: 'date', required: true, message: '请选择日期', trigger: 'change'}
+        ],
+        date2: [
+          {type: 'date', required: true, message: '请选择时间', trigger: 'change'}
+        ],
+        type: [
+          {type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change'}
+        ],
+        resource: [
+          {required: true, message: '请选择活动资源', trigger: 'change'}
+        ],
+        desc: [
+          {required: true, message: '请填写活动形式', trigger: 'blur'}
+        ]
+      },
+      fileList: [{
+        name: 'food.jpeg',
+        url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+      }, {
+        name: 'food2.jpeg',
+        url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+      }],
+      data2: generateData2(),
+      value2: [],
+      filterMethod (query, item) {
+        return item.pinyin.indexOf(query) > -1
+      }
+    }
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    },
+    handleExceed (files, fileList) {
+      this.$message.warning('当前限制选择 3 个文件，本次选择了' + files.length +
+        ' 个文件，共选择了 ' + (files.length + fileList.length) + ' 个文件'
+      )
+    },
+    beforeRemove (file, fileList) {
+      return this.$confirm('确定移除 ' + file.name + '}？')
+    }
+  }
 }
 </script>
 
