de02b6714ab4a0a48bd8283bcd20801874ca4414
linjinze <linjinze999@163.com>
2018-09-04 21:32:15 +0800

    [form]add rich text edit

--- src/pages/functions/forms/FuncFormsEdit.vue
+++ src/pages/functions/forms/FuncFormsEdit.vue
@@ -1,10 +1,38 @@
 <template>
-  <div>富文本编辑器</div>
+  <div>
+    <el-card shadow="hover">
+      请参考<a style="color: #409eff" href="https://www.awesomes.cn/repo/surmon-china/vue-quill-editor">
+      vue-quill-editor</a>。
+    </el-card>
+    <br/>
+    <el-card shadow="hover" style="height: 610px;">
+      <quill-editor
+        v-model="content"
+        ref="myQuillEditor"
+        style="height: 500px;"
+        :options="editorOption">
+      </quill-editor>
+    </el-card>
+  </div>
 </template>
 
 <script>
+import {quillEditor} from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 export default {
-  name: 'FuncFormsEdit'
+  name: 'FuncFormsEdit',
+  components: {
+    quillEditor
+  },
+  data () {
+    return {
+      content: null,
+      editorOption: {}
+    }
+  }
 }
 </script>
 
