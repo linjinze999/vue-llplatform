cd079abf9332e08f453583a2fc3c2920961b9a44
Lin Jinze <linjinze@tp-link.com.cn>
2018-09-06 10:49:13 +0800

    [form]fix form base upload bug

--- src/pages/functions/forms/FuncFormsBase.vue
+++ src/pages/functions/forms/FuncFormsBase.vue
@@ -69,12 +69,8 @@
             <el-upload
               class="upload-demo"
               action="https://jsonplaceholder.typicode.com/posts/"
-              :on-preview="handlePreview"
-              :on-remove="handleRemove"
-              :before-remove="beforeRemove"
               multiple
               :limit="3"
-              :on-exceed="handleExceed"
               :file-list="fileList">
               <el-button size="small" type="primary">点击上传</el-button>
               <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
